442368572ee7ef46564c0ce5edd2b7bd495141c4
FCICoder <mahmoud.aadel1998@gmail.com>
2023-10-10 01:38:04 +0300

    keeping 2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
             <title>Home</title>
         </Helmet>
   <MainSlider/>
-  {/* <CategorySlider/> */}
+  <CategorySlider/>
   <FeaturedProducts/>  
   </>
 }
